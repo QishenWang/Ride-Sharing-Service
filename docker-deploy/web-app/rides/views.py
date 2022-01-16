bf66819ef6da1fc45d550b7e7858f71eca183271
Qishen <qishen_wang@outlook.com>
2022-01-16 17:26:28 -0500

    register page

--- docker-deploy/web-app/rides/views.py
+++ docker-deploy/web-app/rides/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import User, Driver, Ride
 
 
 #def index(request):
@@ -12,6 +15,20 @@ def register(request):
     return render(request, 'rides/register.html')
 
 def register_request(request):
-    return HttpResponse("You're requesting for registration.")
+    try:
+        username_input = request.POST['user_name']
+        password_input = request.POST['user_password']
+        password_repeat_input = request.POST['user_password_repeat']
+        if password_input != password_repeat_input:
+            error_message = "Password does not match the password repeat!"
+            return render(request, 'rides/register.html', {error_message: error_message})
+    except (KeyError):
+        error_message = "Something went wrong..."
+        return render(request, 'rides/register.html', {error_message: error_message})
+    else:
+        new_user = User(user_name=username_input, user_password=password_input)
+        new_user.save()
+        return HttpResponseRedirect(reverse('rides:login'))
+    #return HttpResponse("You're requesting for registration.")
 
 # Create your views here.
