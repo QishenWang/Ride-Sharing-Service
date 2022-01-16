d17c5ec45e74f4b42b7dce5a95ecc5eeafdd43aa
Qishen <qishen_wang@outlook.com>
2022-01-16 16:10:11 -0500

    add new templates to rides

--- docker-deploy/web-app/rides/urls.py
+++ docker-deploy/web-app/rides/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'rides'
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    #path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('register_request/', views.register_request, name='register_request'),
 ]
--- docker-deploy/web-app/rides/views.py
+++ docker-deploy/web-app/rides/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the rides index.")
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the rides index.")
+
+def login(request):
+    return render(request, 'rides/login.html')
+
+def register(request):
+    return render(request, 'rides/register.html')
+
+def register_request(request):
+    return HttpResponse("You're requesting for registration.")
+
 # Create your views here.
