9917bfb05d639bc080d4c4cde748d754f23d38b1
Yutong <yz566@duke.edu>
2022-01-16 20:40:27 -0500

    add error checking for user registration

--- docker-deploy/web-app/rides/models.py
+++ docker-deploy/web-app/rides/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50, unique=True)
     user_password = models.CharField(max_length=50)
 
     def __str__(self):
-        return  str(self.id)+'_'+self.user_name
+        return str(self.id) + '_' + self.user_name
+
 
 VEHICLE_TYPE = [("SEDAN", "SEDAN"), ("SUV", "SUV")]
-MAX_PASSENGER_TYPE = [(1, "1 Passenger"), (2, "2 Passengers"), (3, "3 Passengers"), (4, "4 Passengers"), (5, "5 Passengers")]
+MAX_PASSENGER_TYPE = [(1, "1 Passenger"), (2, "2 Passengers"),
+                      (3, "3 Passengers"), (4, "4 Passengers"),
+                      (5, "5 Passengers")]
+
 
 class Driver(models.Model):
     id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    vehicle_type = models.CharField(max_length=10, choices=VEHICLE_TYPE, default="SUV")
+    vehicle_type = models.CharField(max_length=10,
+                                    choices=VEHICLE_TYPE,
+                                    default="SUV")
     plate_number = models.CharField(max_length=10)
-    max_passenger_number = models.IntegerField(choices=MAX_PASSENGER_TYPE, default=5)
+    max_passenger_number = models.IntegerField(choices=MAX_PASSENGER_TYPE,
+                                               default=5)
     special_vehicle_info = models.TextField(blank=True, default='')
 
     def __str__(self):
@@ -23,21 +31,38 @@ class Driver(models.Model):
 
 
 class Ride(models.Model):
-    ride_owner_id = models.ForeignKey(User, related_name='Owner', on_delete=models.CASCADE)
-    ride_driver_id = models.ForeignKey(Driver, related_name='Driver', on_delete=models.CASCADE, null=True)
-    ride_sharer1_id = models.ForeignKey(User, related_name='Sharer_1', on_delete=models.CASCADE, null=True)
-    ride_sharer2_id = models.ForeignKey(User, related_name='Sharer_2', on_delete=models.CASCADE, null=True)
-    ride_sharer3_id = models.ForeignKey(User, related_name='Sharer_3', on_delete=models.CASCADE, null=True)
-    ride_sharer4_id = models.ForeignKey(User, related_name='Sharer_4', on_delete=models.CASCADE, null=True)
+    ride_owner_id = models.ForeignKey(User,
+                                      related_name='Owner',
+                                      on_delete=models.CASCADE)
+    ride_driver_id = models.ForeignKey(Driver,
+                                       related_name='Driver',
+                                       on_delete=models.CASCADE,
+                                       null=True)
+    ride_sharer1_id = models.ForeignKey(User,
+                                        related_name='Sharer_1',
+                                        on_delete=models.CASCADE,
+                                        null=True)
+    ride_sharer2_id = models.ForeignKey(User,
+                                        related_name='Sharer_2',
+                                        on_delete=models.CASCADE,
+                                        null=True)
+    ride_sharer3_id = models.ForeignKey(User,
+                                        related_name='Sharer_3',
+                                        on_delete=models.CASCADE,
+                                        null=True)
+    ride_sharer4_id = models.ForeignKey(User,
+                                        related_name='Sharer_4',
+                                        on_delete=models.CASCADE,
+                                        null=True)
     ride_destination = models.CharField(max_length=200)
     is_complete = models.BooleanField(default=False)
     is_sharable = models.BooleanField(default=False)
     arrival_time = models.DateTimeField('requested date and time')
     passenger_number = models.IntegerField(default=1)
-    vehicle_type = models.CharField(max_length=10, choices=VEHICLE_TYPE, default="SUV")
+    vehicle_type = models.CharField(max_length=10,
+                                    choices=VEHICLE_TYPE,
+                                    default="SUV")
     special_request = models.TextField(blank=True, default='')
 
     def __str__(self):
         return str(self.id) + '_' + self.ride_owner_id
-
-
--- docker-deploy/web-app/rides/views.py
+++ docker-deploy/web-app/rides/views.py
@@ -4,36 +4,46 @@ from django.urls import reverse
 
 from .models import User, Driver, Ride
 
-
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the rides index.")
+
 
 def login(request):
     return render(request, 'rides/login.html')
 
+
 def register(request):
     return render(request, 'rides/register.html')
+
 
 def register_request(request):
     try:
         #print("hahahhahaha")
         username_input = request.POST.get('user_name')
-        #print(username_input)
         password_input = request.POST.get('user_password')
         password_repeat_input = request.POST.get('user_password_repeat')
+
+        # check for unique user name
+        same_user = User.objects.filter(user_name__exact=username_input)
+        if same_user != None:
+            error_message = "This user name is taken, try another one!"
+            return render(request, 'rides/register.html', {
+                'error_message': error_message,
+            })
         if password_input != password_repeat_input:
             error_message = "Password does not match the password repeat!"
-            print(error_message)
-            return render(request, 'rides/register.html', {'error_message': error_message,})
-        # same_user = User.objects.filter(user_name__exact=username_input)
-        # if same_user
+            return render(request, 'rides/register.html', {
+                'error_message': error_message,
+            })
     except (KeyError):
         error_message = "Something went wrong..."
-        return render(request, 'rides/register.html', {error_message: error_message})
+        return render(request, 'rides/register.html',
+                      {error_message: error_message})
     else:
         new_user = User(user_name=username_input, user_password=password_input)
         new_user.save()
         return HttpResponseRedirect(reverse('rides:login'))
     #return HttpResponse("You're requesting for registration.")
+
 
 # Create your views here.
