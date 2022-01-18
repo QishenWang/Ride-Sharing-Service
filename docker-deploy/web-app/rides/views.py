bb86540b86e0138a2689767cfc74573ca624f70f
Yutong <yz566@duke.edu>
2022-01-17 19:20:28 -0500

    added form for login page

--- docker-deploy/web-app/rides/views.py
+++ docker-deploy/web-app/rides/views.py
@@ -1,30 +1,40 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 from .models import User, Driver, Ride
+from . import forms
+from . import models
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the rides index.")
 
 
 def login(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username)
-        print(password)
-        if password:  # 确保用户名和密码都不为空
-            # 用户名字符合法性验证
-            # 密码长度验证
-            # 更多的其它验证.....
+    if request.method == 'POST':
+        login_form = forms.LoginForm(request.POST)
+        error_message = 'Something went wrong...'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
             try:
                 user = User.objects.get(user_name=username)
             except:
-                return render(request, 'rides/login.html')
+                error_message = 'The username does not exist！'
+                return render(request, 'rides/login.html', locals())
+
             if user.user_password == password:
-                return HttpResponseRedirect(reverse('rides:index'))
-    return render(request, 'rides/login.html')
+                return redirect('/rides/')
+            else:
+                error_message = 'The password is incorrect！'
+                return render(request, 'rides/login.html', locals())
+        else:
+            return render(request, 'rides/login.html', locals())
+
+    login_form = forms.LoginForm()
+    return render(request, 'rides/login.html', locals())
+    # Python内置了一个locals()函数，它返回当前所有的本地变量字典，我们可以偷懒的将这作为render函数的数据字典参数值，就不用费劲去构造一个形如{'message':message, 'login_form':login_form}的字典了
 
 
 def register(request):
@@ -33,14 +43,16 @@ def register(request):
 
 def register_request(request):
     try:
-        #print("hahahhahaha")
+        print("hahahhahaha")
         username_input = request.POST.get('user_name')
         password_input = request.POST.get('user_password')
         password_repeat_input = request.POST.get('user_password_repeat')
 
         # check for unique user name
         same_user = User.objects.filter(user_name__exact=username_input)
-        if same_user != None:
+        print(same_user)
+        if same_user.exists():
+            # query set is not empty, same as "if sam_user:"
             error_message = "This user name is taken, try another one!"
             return render(request, 'rides/register.html', {
                 'error_message': error_message,
@@ -50,7 +62,7 @@ def register_request(request):
             return render(request, 'rides/register.html', {
                 'error_message': error_message,
             })
-    except (KeyError):
+    except:
         error_message = "Something went wrong..."
         return render(request, 'rides/register.html',
                       {error_message: error_message})
